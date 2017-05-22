0cff38295d37a0c489a6aa314bc62f991583169c
zmtrvch <miumiu1078@gmail.com>
2017-05-22 14:01:43 +0300

    Practice 2

---
+++ Assets/Script/DeathHere.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathHere : MonoBehaviour {
+
+	//Стандартна функція, яка викличеться,
+	//коли поточний об’єкт зіштовхнеться із іншим
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		//Намагаємося отримати компонент кролика
+		HeroRabbit rabit = collider.GetComponent<HeroRabbit> ();
+		//Впасти міг не тільки кролик
+		if(rabit != null) {
+			//Повідомляємо рівень, про смерть кролика
+			LevelController.current.onRabitDeath (rabit);
+		}
+	}
+}
